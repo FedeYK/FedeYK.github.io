e147823a80e07820265ca722944d127e3bbe35f4
fede <frbarca@gmail.com>
2023-03-17 00:56:07 +0100

    added background picture

--- javascript/script.js
+++ javascript/script.js
@@ -1,7 +1,7 @@
-const words = ["javascript", "python", "ruby", "java", "cplusplus"];
-let selectedWord = "";
-let displayWord = "";
-let lives = 7;
+// const words = ["javascript", "python", "ruby", "java", "cplusplus"];
+// let selectedWord = "";
+// let displayWord = "";
+// let lives = 7;
 
 const wordEl = document.getElementById("word");
 const messageEl = document.getElementById("message");
@@ -10,23 +10,49 @@ const resetBtn = document.getElementById("reset");
 
 resetBtn.addEventListener("click", resetGame);
 
+
+// async function randomWord() {
+//     const difficulty = document.getElementById("difficulty").value;
+//     const response = await fetch(`https://random-word-api.herokuapp.com/word?number=1&length=${getLengthByDifficulty(difficulty)}`);
+//     const data = await response.json();
+//     return data[0];
+// }
+
+// function getLengthByDifficulty(difficulty) {
+//     switch (difficulty) {
+//         case "easy":
+//             return "4,6";
+//         case "medium":
+//             return "7,9";
+//         case "hard":
+//             return "10,20";
+//         default:
+//             return "4,6";
+//     }
+// }
+
+
 async function randomWord() {
-    const response = await fetch("https://random-word-api.herokuapp.com/word?number=1");
+    // I have changed the API to return only words with 5 letters.
+    // Have been trying it and if not is too difficult.
+    const response = await fetch("https://random-word-api.herokuapp.com/word?length=5");
     const data = await response.json();
-    return data[0];
+    // I use toUpperCase() so the word is always in uppercase. I also use the button letters in uppercase.
+    return data[0].toUpperCase();
 }
 
 function displayKeyboard() {
     for (let i = 65; i <= 90; i++) {
         const btn = document.createElement("button");
         btn.classList.add("btn", "btn-secondary");
-        btn.textContent = String.fromCharCode(i).toLowerCase();
+        btn.textContent = String.fromCharCode(i);
         btn.addEventListener("click", handleKeyPress);
         keyboardEl.appendChild(btn);
     }
 }
 
 function displayMaskedWord() {
+    // I use replace() to replace all the letters with "_"
     displayWord = selectedWord.replace(/./g, "_");
     wordEl.textContent = displayWord;
 }
@@ -37,9 +63,10 @@ function handleKeyPress(e) {
 
     if (selectedWord.includes(letter)) {
         for (let i = 0; i < selectedWord.length; i++) {
-            if (selectedWord[i] === letter) {
+            if (selectedWord[i] == letter) {
                 displayWord = displayWord.substring(0, i) + letter + displayWord.substring(i + 1);
             }
+            e.target.classList = "btn btn-success";
         }
         wordEl.textContent = displayWord;
 
@@ -50,6 +77,7 @@ function handleKeyPress(e) {
     } else {
         lives--;
         updateHangmanImage();
+        e.target.classList = "btn btn-danger";
 
         if (lives === 0) {
             messageEl.textContent = "You lost! The word was: " + selectedWord;
@@ -76,6 +104,7 @@ async function resetGame() {
     const buttons = document.querySelectorAll("#keyboard button");
     buttons.forEach((btn) => {
         btn.disabled = false;
+        btn.classList = "btn btn-secondary";
     });
 
     updateHangmanImage();
