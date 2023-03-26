d7c2d745324d876adafa0e35a9c5143034480ab9
fede <frbarca@gmail.com>
2023-03-26 01:32:32 +0100

    version 260323

--- javascript/script.js
+++ javascript/script.js
@@ -1,84 +1,92 @@
-// const words = ["javascript", "python", "ruby", "java", "cplusplus"];
-// let selectedWord = "";
-// let displayWord = "";
-// let lives = 7;
+// Description: This is the JavaScript file for the Hangman game.
 
+// Global variables
 const wordEl = document.getElementById("word");
 const messageEl = document.getElementById("message");
 const keyboardEl = document.getElementById("keyboard");
 const resetBtn = document.getElementById("reset");
+const difficultyEl = document.getElementById("difficulty");
 
+// Event listeners for the reset button and the difficulty select.
 resetBtn.addEventListener("click", resetGame);
+difficultyEl.addEventListener("change", resetGame);
 
 
-// async function randomWord() {
-//     const difficulty = document.getElementById("difficulty").value;
-//     const response = await fetch(`https://random-word-api.herokuapp.com/word?number=1&length=${getLengthByDifficulty(difficulty)}`);
-//     const data = await response.json();
-//     return data[0];
-// }
+// Function to get the length of the word based on the difficulty.
+function getLengthByDifficulty(difficulty) {
+    switch (difficulty) {
+        case "easy":
+            return "4";
+        case "medium":
+            return "6";
+        case "hard":
+            return "8";
+        case "extreme":
+            return "10";
+        case "impossible":
+            return "12";
+    }
+}
 
-// function getLengthByDifficulty(difficulty) {
-//     switch (difficulty) {
-//         case "easy":
-//             return "4,6";
-//         case "medium":
-//             return "7,9";
-//         case "hard":
-//             return "10,20";
-//         default:
-//             return "4,6";
-//     }
-// }
-
-
+// Function to get a random word from the API.
 async function randomWord() {
-    // I have changed the API to return only words with 5 letters.
-    // Have been trying it and if not is too difficult.
-    const response = await fetch("https://random-word-api.herokuapp.com/word?length=5");
+    const difficulty = document.getElementById("difficulty").value;
+    const response = await fetch(`https://random-word-api.herokuapp.com/word?length=${getLengthByDifficulty(difficulty)}&lang=en`);
     const data = await response.json();
-    // I use toUpperCase() so the word is always in uppercase. I also use the button letters in uppercase.
     return data[0].toUpperCase();
 }
 
+// Function to display the keyboard.
 function displayKeyboard() {
+    // Use a for loop to create the buttons for the keyboard.
+    // Use the ASCII code to get the letters. 65 is the code for "A" and 90 is the code for "Z".
     for (let i = 65; i <= 90; i++) {
+        // Create the button element.
         const btn = document.createElement("button");
         btn.classList.add("btn", "btn-secondary");
         btn.textContent = String.fromCharCode(i);
         btn.addEventListener("click", handleKeyPress);
+        // Append the created button to the keyboard div.
         keyboardEl.appendChild(btn);
     }
 }
 
+// Function to display the masked word.
 function displayMaskedWord() {
-    // I use replace() to replace all the letters with "_"
+    // replace() to replace all the letters with "_"
     displayWord = selectedWord.replace(/./g, "_");
     wordEl.textContent = displayWord;
 }
 
+// Function to handle the key press.
 function handleKeyPress(e) {
     const letter = e.target.textContent;
     e.target.disabled = true;
 
+    // Check if the selected word includes the letter.
     if (selectedWord.includes(letter)) {
+        // Use a for loop to check the places the letter is in the word.
         for (let i = 0; i < selectedWord.length; i++) {
             if (selectedWord[i] == letter) {
                 displayWord = displayWord.substring(0, i) + letter + displayWord.substring(i + 1);
             }
+            // Change the class of the button to btn-success.
             e.target.classList = "btn btn-success";
         }
+        // Update the word element. Show the letter.
         wordEl.textContent = displayWord;
 
+        // Check if the word is complete and if it is the player wins.
         if (displayWord === selectedWord) {
             messageEl.textContent = "You won!";
             disableKeyboard();
         }
+    // If the word doesn't include the letter, the player loses a life.
     } else {
         lives--;
         updateHangmanImage();
         e.target.classList = "btn btn-danger";
-
+        // Check if the player has any lives left. If not, the player loses.
         if (lives === 0) {
             messageEl.textContent = "You lost! The word was: " + selectedWord;
             disableKeyboard();
@@ -88,6 +96,7 @@ function handleKeyPress(e) {
     }
 }
 
+// Function to disable the keyboard when the game is over.
 function disableKeyboard() {
     const buttons = document.querySelectorAll("#keyboard button");
     buttons.forEach((btn) => {
@@ -95,6 +104,7 @@ function disableKeyboard() {
     });
 }
 
+// Function to reset the game and get a new word.
 async function resetGame() {
     selectedWord = await randomWord();
     displayMaskedWord();
@@ -111,11 +121,13 @@ async function resetGame() {
     updateHangmanImage();
 }
 
+// Function to update the hangman image.
 function updateHangmanImage() {
     const hangmanImg = document.getElementById("hangman");
     const wrongGuesses = 7 - lives;
     hangmanImg.src = `images/${wrongGuesses}.png`;
 }
 
+// Call the functions to run the game.
 displayKeyboard();
 resetGame();
